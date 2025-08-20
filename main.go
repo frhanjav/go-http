910bbd6c12964fcf2f219d263fc403874c115690
frhanjav <farhan.javed006@gmail.com>
2025-08-20 15:20:57 +0530

    channel-read

--- main.go
+++ main.go
@@ -8,39 +8,57 @@ import (
 	"os"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		buf := make([]byte, 8) 
+		var line string
+
+		for {
+			n, err := f.Read(buf)
+
+			readData := buf[:n]
+
+			if i := bytes.IndexByte(readData, '\n'); i != -1 {
+				line += string(readData[:i])
+				out <- line
+				line = string(readData[i+1:])
+			} else {
+				line += string(readData)
+			}
+
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				fmt.Printf("Error reading from file: %v\n", err)
+				os.Exit(1)
+			}
+		}
+
+		if len(line) > 0 {
+			out <- line
+		}
+	}()
+
+	return out
+
+}
+
 func main() {
 	f, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal("error", err)
 	}
-	defer f.Close()
 
-	buf := make([]byte, 8) 
-	var line string
-
-	for {
-		n, err := f.Read(buf)
-
-		readData := buf[:n]
-
-		if i := bytes.IndexByte(readData, '\n'); i != -1 {
-			line += string(readData[:i])
-			fmt.Printf("read: %s\n", line)
-			line = string(readData[i+1:])
-		} else {
-			line += string(readData)
-		}
-
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			fmt.Printf("Error reading from file: %v\n", err)
-			os.Exit(1)
-		}
+	words := getLinesChannel(f)
+	for word := range words {
+		fmt.Printf("read: %s\n", word)
 	}
 
-	if len(line) > 0 {
-		fmt.Printf("read: %s\n", line)
-	}
+
 }
