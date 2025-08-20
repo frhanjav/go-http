1ad8ecea5ef3a7914036adfcb2eb541a5b46e464
frhanjav <farhan.javed006@gmail.com>
2025-08-20 14:36:40 +0530

    const-read-buffer-size

--- main.go
+++ main.go
@@ -9,36 +9,38 @@ import (
 )
 
 func main() {
-	f, err:= os.Open("messages.txt")
+	f, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal("error", err)
 	}
 	defer f.Close()
 
-	data := make([]byte, 8)
-	var line string	
+	buf := make([]byte, 8) 
+	var line string
+
 	for {
-		n, err := f.Read(data)
-		data = data[:n]
+		n, err := f.Read(buf)
 
-		if err != nil && err != io.EOF {
-			fmt.Printf("Error reading from file: %v\n", err)
-			os.Exit(1)
-		}
+		readData := buf[:n]
 
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			line += string(data[:i])
+		if i := bytes.IndexByte(readData, '\n'); i != -1 {
+			line += string(readData[:i])
 			fmt.Printf("read: %s\n", line)
-			line = string(data[i+1:])
+			line = string(readData[i+1:])
 		} else {
-			line += string(data)
+			line += string(readData)
 		}
 
 		if err == io.EOF {
 			break
 		}
+		if err != nil {
+			fmt.Printf("Error reading from file: %v\n", err)
+			os.Exit(1)
+		}
 	}
+
 	if len(line) > 0 {
 		fmt.Printf("read: %s\n", line)
 	}
-}	
+}
